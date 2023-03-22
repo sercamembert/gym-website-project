dbc5718e1a23dc10141d2f986b8ec4c8607eb4d4
Filip Maszota <filipmaszota3@gmail.com>
2023-03-22 20:25:28 +0100

    add video to welcome page

--- js/link.js
+++ js/link.js
@@ -1,8 +1,16 @@
-const ellipseTop = document.querySelector('.ellipse--top')
+if(window.innerWidth>=768){
+    const ellipseTop = document.querySelector('.ellipse--top')
 const ellipseBottom = document.querySelector('.ellipse--bottom')
 document.querySelector('.welcome__par').addEventListener('click',event=>{
     event.preventDefault();
     ellipseTop.classList.add('elipse-end-top')
     ellipseBottom.classList.add('elipse-end-bottom')
-    window.location.href = 'main.html';
+    setTimeout(()=>{
+        window.location.href = 'main.html';
+    },1000)
+
 })
+}else{
+    window.location.href = 'main.html';
+}
+
